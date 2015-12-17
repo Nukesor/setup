090375c4db21e7b993f6beacbb2ae0c67ebe3958
Arne Beer <arne@twobeer.de>
2015-12-17 09:53:28 +0100

    Packagelists

--- config.sh
+++ config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export hdd=/dev/sda
-export label=root
-export bootlabel=boot
+export hdd=/dev/sdb
+export label=usbroot
+export bootlabel=usbboot
 export boot=${hdd}1
 export root=${hdd}2
 #export root=/dev/mapper/$crypt
