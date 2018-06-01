144e1c395924bd26b02809a90ec7dc8f3fb1c7a2
Arne Beer <arne@twobeer.de>
2018-06-01 23:36:50 +0200

    Crypt mkinitcpio. Remove old package

--- inside/efi.sh
+++ inside/efi.sh
@@ -6,7 +6,12 @@ source ./config.sh
 pacman -S intel-ucode
 bootctl --path=/boot install
 cp -r files/loader /boot/
-cp -r files/mkinitcpio.conf.efi /etc/mkinitcpio.conf
+
+if $crypt; then
+    cp -r files/mkinitcpio.conf.efi.crypt /etc/mkinitcpio.conf
+else
+    cp -r files/mkinitcpio.conf.efi /etc/mkinitcpio.conf
+fi
 
 # Set cryptdevice for boot
 if $crypt; then
