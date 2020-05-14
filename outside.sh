b2adf60fc336ad7bc519bfa686ba8343ac713136
Arne Beer <contact@arne.beer>
2020-05-14 20:32:04 +0200

    Remove prerequisite

--- outside.sh
+++ outside.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-./outside/prerequisite.sh
 ./outside/partition.sh
 ./outside/format.sh
 ./outside/install.sh
--- outside/prerequisite.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source ./config.sh
-
-# Install requirements to ram, as we might not have a writable fs.
-mkdir -p $pacmanroot/var/lib/pacman
-mkdir -p $pacmancache
-
-pacman --cachedir $pacmancache --sysroot $pacmanroot -Sy --noconfirm --needed dosfstools gptfdisk base-devel
