2cec962bc07f190253001845db299c696071f8b9
Arne Beer <contact@arne.beer>
2020-05-14 20:27:45 +0200

    Fix variable fuckup

--- config.sh
+++ config.sh
@@ -7,14 +7,17 @@ pacmancache=/tmp/chache
 export hdd=/dev/sda
 export rootlabel=root
 export bootlabel=boot
-export boot="${hdd}1"
-export root="${hdd}2"
+export boot_partition="${hdd}1"
+export root_partition="${hdd}2"
 export crypt=true
 
 if $crypt; then
     export cryptname=cryptroot
     export root=/dev/mapper/$cryptname
+else
+    export root=$root_partition
 fi
+
 
 # Inside config
 export databases=true
--- outside/format.sh
+++ outside/format.sh
@@ -5,20 +5,20 @@ source ./config.sh
 
 if $crypt; then
     # Filesystem
-    cryptsetup -y -v luksFormat ${root}
-    cryptsetup open ${root} $cryptname
+    cryptsetup -y -v luksFormat ${root_partion}
+    cryptsetup open ${root_partion} $cryptname
 fi
 
 # Create file systems
 echo "y
-"|mkfs.btrfs -L root $root
+"|mkfs.msdos -F 32 $boot_partition
 echo "y
-"|mkfs.msdos -F 32 $boot
+"|mkfs.btrfs -L root $root
 
 #Mounting
 mount $root /mnt
 mkdir -p /mnt/boot
-mount $boot /mnt/boot
+mount $boot_partition /mnt/boot
 
 # Create required directories
 mkdir -p /mnt/run
