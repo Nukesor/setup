fb555fe1c8fe15895ffae17e2d0ac6dd8f4f2c22
Arne Beer <contact@arne.beer>
2020-05-15 16:25:48 +0200

    Rust and watchexec stuff

--- inside/user.sh
+++ inside/user.sh
@@ -6,3 +6,5 @@ usermod -a -G tty,wheel,uucp,http,video,audio,nuke,network,docker nuke
 # Set shell
 chsh -s /usr/bin/zsh nuke
 sudo -u nuke rustup default stable
+
+cargo install cargo-watch cargo-outdated cargo-release cargo-update
