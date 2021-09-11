f83c838835f1e62dfe9ac3c7efe5efc331672e4b
Arne Beer <contact@arne.beer>
2021-09-11 12:54:22 +0200

    Fully move to paru

--- inside/pkg.sh
+++ inside/pkg.sh
@@ -5,14 +5,13 @@ source ./config.sh
 pacman -Rns vi --noconfirm
 pacman -Syy --noconfirm --needed $(cat pkglist)
 
-# Add yay
-su nuke -c "git clone https://aur.archlinux.org/yay.git /tmp/yay"
-su nuke -c "cd /tmp/yay && makepkg"
-pacman -U /tmp/yay/*.pkg.tar.xz --noconfirm
-pacman -U /tmp/yay/*.pkg.tar.zst --noconfirm
+# Add paru
+su nuke -c "git clone https://aur.archlinux.org/paru.git /tmp/paru"
+su nuke -c "cd /tmp/paru && makepkg"
+pacman -U /tmp/paru/*.pkg.tar.zst --noconfirm
 
 # Install aur packages
 while read line; do
     echo $line
-    su nuke -c "yay -Sy --noconfirm --needed ${line}"
+    su nuke -c "paru -Sy --noconfirm --needed ${line}"
 done < ./aur-pkglist
