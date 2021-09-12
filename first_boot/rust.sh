83aea33b3274b1ca792185ff91b7e349f5e0f63a
Arne Beer <contact@arne.beer>
2021-09-12 11:31:53 +0200

    Use cargo pacman packages

--- first_boot/rust.sh
+++ first_boot/rust.sh
@@ -2,7 +2,5 @@
 rustup default nightly
 rustup component add rustfmt-preview
 rustup component add rls-preview rust-analysis rust-src
-cargo install cargo-watch
 cargo install cargo-update
-cargo install cargo-outdated
 cargo install cargo-license
