ab44f1666ed19e05f8ce911bb3360e7b8a72df92
Arne Beer <contact@arne.beer>
2024-05-31 23:24:52 +0200

    remove: Config files, migrate to bois

--- inside/aur.sh
+++ inside/aur.sh
@@ -2,18 +2,17 @@
 set -euo pipefail
 source ./config.sh
 
-if ! command -v paru &> /dev/null; then
-	# Add paru
-	echo "Clone paru"
-	su $USERNAME -c "cd /tmp && git clone https://aur.archlinux.org/paru-bin.git /tmp/paru"
-	echo "Build paru"
-	su $USERNAME -c "cd /tmp/paru && makepkg"
-	echo "Install paru"
-	pacman -U /tmp/paru/*.pkg.tar.zst --noconfirm
+if ! command -v paru &>/dev/null; then
+    # Add paru
+    echo "Clone paru"
+    su $USERNAME -c "cd /tmp && git clone https://aur.archlinux.org/paru-bin.git /tmp/paru"
+    echo "Build paru"
+    su $USERNAME -c "cd /tmp/paru && makepkg"
+    echo "Install paru"
+    pacman -U /tmp/paru/*.pkg.tar.zst --noconfirm
 fi
 
-echo "Install aur packages"
-while read line; do
-    echo "Installing $line"
-    su $USERNAME -c "paru -Sy --noconfirm --needed ${line}"
-done < ./aur-pkglist
+sudo useradd --home-dir /var/lib/aur --create-home aur
+
+cp files/sudoers /etc/sudoers
+chmod 440 /etc/sudoers
--- inside/pkg.sh
+++ inside/pkg.sh
@@ -3,7 +3,5 @@ set -euo pipefail
 source ./config.sh
 
 echo "Starting pkg setup step"
-# Install system packages
-pacman -Syy --noconfirm --needed $(cat pkglist)
 
 echo "Finished pkg setup step"
